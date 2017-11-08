39cfa516700032eccda50dd72369a4e1cce64c00
KarolinLuneburg <32455233+karolin@users.noreply.github.com>
2017-11-08 20:23:22 +0100

    comments

--- EventApp/user.php
+++ EventApp/user.php
@@ -1,98 +1,74 @@
-<?php 
+<?php
+//-- PAGE SETUP ----------------------------------------------------------------
+
+//-- CHECK IF USER IS LOGGED IN
+session_start();
+    if (!isset($_SESSION['userID'])) {
+        header("Location:index.php");
+    }
+
+//-- INCLUDE
 include("header.php");
 include("menu.php");
 include("userinfo.php");
 
+//-- DATABASE CONNECTION
+@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+if ($db->connect_error) {
+   echo "could not connect: " . $db->connect_error;
+   header("Location: index.php");
+   exit();
+}
+$userID = $_SESSION['userID'];
 ?>
-    <div style="margin-top: 150px;" class="allEvents">
-      <?php 
-        //session_start();
-        $userID = $_SESSION['userID'];
-        //echo $userID;
-        
-        @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-        
-        if ($db->connect_error) {
-            echo "could not connect: " . $db->connect_error;
-            exit();
-        }
-        
-        
-        if (isset($_POST['minus'])){
-    
-    //@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-    
+
+<!-- EVENT CARDS -------------------------------------------------------------->
+<div style="margin-top: 150px;" class="allEvents">
+<?php
+  //-- DELETE EVENT ------------------------------------------------------------
+  if (isset($_POST['minus'])){
     $eventid = $_POST['eventID'];
-    //$userid = $_SESSION['userID'];
-             
-    //get the eventID from the input and the userID from the session and insert it into the database
+
     $deleteQuery = "DELETE FROM Attend WHERE userID = '{$userID}' AND eventID = '{$eventid}' ";
     $stmt = $db->prepare($deleteQuery);
     $stmt->execute();
-             
+  }
 
-}
-        
-        $query = "SELECT Events.eventID, Events.title, Events.description, Events.startdate, Events.enddate, Events.time, Events.price, Events.location, Events.image, Events.link, Events.host FROM Events 
-        JOIN Attend on Events.eventID = Attend.eventID
-        JOIN Users on Users.userID = Attend.userID
-        WHERE Users.userID = '{$userID}'";
-        
+  //-- JOIN TO GET EVENTS USER ATTENDED ----------------------------------------
+  $query = "SELECT Events.eventID, Events.title, Events.description, Events.startdate, Events.enddate, Events.time, Events.price, Events.location, Events.image, Events.link, Events.host FROM Events
+  JOIN Attend on Events.eventID = Attend.eventID
+  JOIN Users on Users.userID = Attend.userID
+  WHERE Users.userID = '{$userID}'";
 
-       
-        
-        $stmt = $db->prepare($query);
-        //$stmt->bind_param('i', $userID);
-        $stmt->bind_result($eventID, $title, $description, $startdate, $enddate, $time, $price, $location, $image, $link, $host);
-        $stmt->execute();
-        while($stmt->fetch()){ ?>
-            
-       <!---------------------------------EVENT ONE-->
-       <div class="eventContainerOne">
-                <!----------------------------------------event img-->
-                <div class="imgContainer" style="background-image: url('uploadedfiles/<?php echo "$image"; ?>');"></div>
-                <!---------------------------------------attend Bnt-->
-                <form method="POST" action='user.php'>
-                      <input type="submit" value="-" class="plusBtn" name="minus">
-                      <input type="hidden" value="<?php echo "$eventID"; ?>" name="eventID">
-                  </form>
-                <!--------------------------------event information-->
-                <div class="infoContainer">
-                  <p class="eventTitle">
-                     <?php 
-                        echo "<h4>$title</h4> <p><strong>Date:</strong> $startdate</p> <p><strong>Time: </strong> $time</p>";
-                      ?>
+  $stmt = $db->prepare($query);
+  $stmt->bind_result($eventID, $title, $description, $startdate, $enddate, $time, $price, $location, $image, $link, $host);
+  $stmt->execute();
+  while($stmt->fetch()){ ?>
 
-                  </p>
-                  <!---------------------------------expander btn--> 
-                  <a href="#" class="expanderBtn">
-                      <i class="fa fa-angle-down" aria-hidden="true"></i>
-                  </a>
-                  <!----------------------------event description-->
-                  <p class="eventDescription">
-                    <?php echo "$description";?>
-                  </p>  
-                </div>
-                </div>
-       
-       <?php  } ?>
-
-       </div>
-       
-
-       
-  
-
-
+ <div class="eventContainerOne">
+      <div class="imgContainer" style="background-image: url('uploadedfiles/<?php echo "$image"; ?>');"></div>
+      <form method="POST" action='user.php'>
+            <input type="submit" value="-" class="plusBtn" name="minus">
+            <input type="hidden" value="<?php echo "$eventID"; ?>" name="eventID">
+        </form>
+      <div class="infoContainer">
+        <p class="eventTitle">
+           <?php
+              echo "<h4>$title</h4> <p><strong>Date:</strong> $startdate</p> <p><strong>Time: </strong> $time</p>";
+            ?>
+        </p>
+        <a href="#" class="expanderBtn">
+            <i class="fa fa-angle-down" aria-hidden="true"></i>
+        </a>
+        <p class="eventDescription">
+          <?php echo "$description";?>
+        </p>
+      </div>
+  </div>
+ <?php  } ?>
+ </div>
 
 
 <?php 
-
-
-
-
-
-
-
 include("footer.php");
 ?>
--- EventApp/userinfo.php
+++ EventApp/userinfo.php
@@ -1,44 +1,37 @@
 <?php
-  @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+//-- PAGE SETUP ----------------------------------------------------------------
 
-        if ($db->connect_error) {
-            echo "could not connect: " . $db->connect_error;
-            exit();
-        }
+//-- DATABASE CONNECTION
+@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+if ($db->connect_error) {
+   echo "could not connect: " . $db->connect_error;
+   header("Location: index.php");
+   exit();
+}
 
-    //start session to be able to identify the user id
-   // session_start();
-
-    //get userID
-    $userID = $_SESSION['userID'];
-    //echo "$userID";
+$userID = $_SESSION['userID'];
 
 
-    //get the image from the db where the user is the same as the user logged in
-    $userquery = "SELECT type, image FROM Users WHERE userID = '{$userID}' ";
-    $stmt = $db->prepare($userquery);
-    $stmt->bind_result($type, $image);
-    $stmt->execute();
+//-- GET USER IMAGE ------------------------------------------------------------
+$userquery = "SELECT type, image FROM Users WHERE userID = '{$userID}' ";
+$stmt = $db->prepare($userquery);
+$stmt->bind_result($type, $image);
+$stmt->execute();
+
 while($stmt->fetch()){
-
-    if ($type == 'organisation' || $type == 'student'){
-    ?>
-        <div class="userInfo">
-            <div class="userImg" style="background-image: url('uploadedfiles/<?php echo "$image"; ?>');"></div>
-
-            <div class="userWelcome">Hi <?php
-                echo $_SESSION['username'];
-                echo $_SESSION['organisation'];
-                ?>! Welcome to your page.
-            </div>
-        </div>
-
+  if ($type == 'organisation' || $type == 'student'){?>
+      <div class="userInfo">
+          <div class="userImg" style="background-image: url('uploadedfiles/<?php echo "$image"; ?>');"></div>
+          <div class="userWelcome">Hi <?php
+              echo $_SESSION['username'];
+              echo $_SESSION['organisation'];
+              ?>! Welcome to your page.
+          </div>
+      </div>
 <?php
-    }else if ($type == 'admin'){
-?>
+    }else if ($type == 'admin'){?>
         <div class="userInfo">
             <div class="userImg" style="background-image: url('../uploadedfiles/<?php echo "$image"; ?>');"></div>
-
             <div class="userWelcome">Hi <?php
                 echo $_SESSION['username'];
                 echo $_SESSION['organisation'];
